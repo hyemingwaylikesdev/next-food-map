a66f698c3555d7535d098899d6a976ae65d0fbc1
hyemin park <hyeminpark@hyeminui-iMac.local>
2023-09-09 21:37:08 +0900

    feedback page

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,17 +1,49 @@
 import { Fragment } from 'react';
+import { GetServerSideProps, NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 
 import Header from '../components/common/Header';
+import FeedbackSection from '../components/feedback/FeedbackSection';
+import { getFeedbackListFromFirestore } from '../firebase/feedback';
+import type { Feedback } from '../types/feedback';
 
-export default function Home() {
+interface Props {
+  initialFeedbackList: Feedback[];
+}
+
+export const FeedbackPage: NextPage<Props> = ({ initialFeedbackList }) => {
   return (
     <Fragment>
       <NextSeo
-        title="추가 의견"
-        description="추천해주시고 싶은 가게를 알려주세요"
+        title="피드백"
+        description="매장 지도 서비스에 대한 피드백을 받습니다."
         canonical="https://mong-sik-hyemingwaylikesdev.vercel.app/feedback"
+        openGraph={{
+          url: 'https://mong-sik-hyemingwaylikesdev.vercel.app/feedback',
+        }}
       />
       <Header />
+
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          touchAction: 'pinch-zoom',
+        }}
+      >
+        <FeedbackSection initialFeedbackList={initialFeedbackList} />
+      </main>
     </Fragment>
   );
-}
+};
+export default FeedbackPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      initialFeedbackList: await getFeedbackListFromFirestore(),
+    },
+  };
+};
